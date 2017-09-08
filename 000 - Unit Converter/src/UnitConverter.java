5024d55b867e69a8d2b09b6e58500f1c3f96ddd8
sawraf-Vostro-3559 <sawickirafal@outlook.com>
2017-09-08 10:42:47 +0200

    - usage changed - Unit interface added - started coding LengthUnit class

--- 000 - Unit Converter/src/UnitConverter.java
+++ 000 - Unit Converter/src/UnitConverter.java
@@ -1,7 +1,39 @@
+interface Unit {
+    double getResult();
+}
+
+class LengthUnit implements Unit {
+    private double result;
+
+    LengthUnit(double quantity, String startingUnit, String endingUnit) {
+        double startExponent = 0;
+        switch (startingUnit.toLowerCase()) {
+            case "kilometres":
+                startExponent = 3;
+                break;
+            case "decimetres":
+                startExponent = -1;
+                break;
+            case "centimetres":
+                startExponent = -2;
+                break;
+        }
+    }
+
+    @Override
+    public double getResult() {
+        return 0;
+    }
+}
+
 public class UnitConverter {
 
     private static void usage() {
-        System.out.println("quantity startingUnit endingUnit");
+
+        System.out.println("quantity x startingUnit endingUnit");
+        System.out.println("where x is one of the following:");
+        System.out.println("     a - area units");
+        System.out.println("     l - length units");
     }
 
     public static void main(String[] args) {
