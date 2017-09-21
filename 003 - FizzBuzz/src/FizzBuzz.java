afc6b822061d05b5b3e2e72e7ab2a65c3914ee28
sawraf-Vostro-3559 <sawickirafal@outlook.com>
2017-09-21 21:20:47 +0200

    003 - FizzBuzz >Problem solved.

--- 003 - FizzBuzz/src/FizzBuzz.java
+++ 003 - FizzBuzz/src/FizzBuzz.java
@@ -1,5 +1,14 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-
+        for (int i = 1; i < 101; i++) {
+            String output = "";
+            if (i % 3 == 0)
+                output += "Fizz";
+            if (i % 5 == 0)
+                output += "Buzz";
+            if (output.equals(""))
+                output = Integer.toString(i);
+            System.out.println(i + ": " + output);
+        }
     }
 }
