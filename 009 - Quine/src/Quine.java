1c4fa4438b6584ded9a91ff45442be10c593bd04
sawraf-Vostro-3559 <sawickirafal@outlook.com>
2017-09-21 20:56:51 +0200

    009 - Quine >Done. Problem solved

--- 009 - Quine/src/Quine.java
+++ 009 - Quine/src/Quine.java
@@ -1,5 +1,36 @@
 public class Quine {
     public static void main(String[] args) {
-
+        char comma = 44;
+        char tab = 9;
+        char q = 34;
+        String[] output = {
+                "public class Quine {",
+                "   public static void main (String[] args) {",
+                "       char comma = 44;",
+                "       char tab = 9;",
+                "       char q = 34;",
+                "       String[] output = {",
+                "       };",
+                "       for (int i = 0; i < 6; i++)",
+                "           System.out.println(output[i]);",
+                "       for (String out : output) {",
+                "           for (int i = 4; i > 0; i--)",
+                "               System.out.print(tab);",
+                "           System.out.println(q + out + q + comma);",
+                "       }",
+                "       for (int i = 6; i < output.length; i++)",
+                "           System.out.println(output[i]);",
+                "   }",
+                "}",
+        };
+        for (int i = 0; i < 6; i++)
+            System.out.println(output[i]);
+        for (String out : output) {
+            for (int i = 4; i > 0; i--)
+                System.out.print(tab);
+            System.out.println(q + out + q + comma);
+        }
+        for (int i = 6; i < output.length; i++)
+            System.out.println(output[i]);
     }
 }
